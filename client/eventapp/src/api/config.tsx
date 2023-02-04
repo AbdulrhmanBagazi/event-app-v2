332f537a8062f52407238c3666eca19f50e35170
abdulrhman bagazi <bagazi.dev@gmail.com>
2023-02-04 07:51:32 +0300

    testing git push for this file

--- client/eventapp/src/api/config.tsx
+++ client/eventapp/src/api/config.tsx
@@ -1,4 +1,10 @@
-export const AuthApi = 'http://192.168.100.14:4000/client';
-export const GRAPHQL = 'http://192.168.100.14:4000/graphql/client';
-export const PUBLIC_GRAPHQL =
-  'http://192.168.100.14:4000/graphql/public_client';
+// export const AuthApi = 'http://192.168.100.14:4000/client';
+// export const GRAPHQL = 'http://192.168.100.14:4000/graphql/client';
+// export const PUBLIC_GRAPHQL =
+//   'http://192.168.100.14:4000/graphql/public_client';
+
+export const AuthApi = 'http://172.20.10.6:4000/client';
+export const GRAPHQL = 'http://172.20.10.6:4000/graphql/client';
+export const PUBLIC_GRAPHQL = 'http://172.20.10.6:4000/graphql/public_client';
+
+//"react-native-reanimated": "^3.0.0-rc.3",
